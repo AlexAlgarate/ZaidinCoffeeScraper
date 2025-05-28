e748db8ce051a4535b6e1ebdb96a759fdf2aa6b8
alex <alex.algarate@gmail.com>
2025-05-28 21:24:18 +0200

    Remove unused exception classes related to coffee domain

--- src/domain/exceptions/coffee_exceptions.py
+++ src/domain/exceptions/coffee_exceptions.py
@@ -10,30 +10,6 @@ class CoffeeScrapingError(CoffeeException):
     pass
 
 
-class InvalidPriceError(CoffeeException):
-    """Raised when price cannot be parsed or is invalid"""
-
-    pass
-
-
-class InvalidWeightError(CoffeeException):
-    """Raised when weight cannot be determined"""
-
-    pass
-
-
-class ProductNotFoundError(CoffeeException):
-    """Raised when a product cannot be found"""
-
-    pass
-
-
-class DetailExtractionError(CoffeeException):
-    """Raised when product details cannot be extracted"""
-
-    pass
-
-
 class PriceParsingError(Exception):
     def __init__(self, price_text: str, format_text: str, original: Exception):
         msg = f"Error parsing price '{price_text}' with format '{format_text}'"
